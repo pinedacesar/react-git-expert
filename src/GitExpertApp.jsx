b9a2640ff2140341d19b0e41028690d257081e96
pineda04 <cesar.pineda.assert@gmail.com>
2023-07-23 17:05:23 -0300

    Primer commit

---
+++ src/GitExpertApp.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GitExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch']);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    // console.log(onNewCategory);
+    // console.log('Valorant');
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GitExpertApp</h1>
+
+      <AddCategory
+        // setCategories={setCategories}
+        onNewCategory={onAddCategory}
+      />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+      {/* <li>ABC</li> */}
+    </>
+  );
+};
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+import { getGifs } from '../helpers/GetGis';
+
+export const useFetchGifs = (category) => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+  return {
+    images,
+    isLoading,
+  };
+};
